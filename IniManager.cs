10555ccc7f6645848c6fb6833d44a2582120f334
wins <ga179@yandex.ru>
2020-05-15 14:53:43 +0500

    base commit

---
+++ IniManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace wp.dll.iniManager
+{
+    public class IniManager
+    {
+        private string Path { get; } = AppDomain.CurrentDomain.BaseDirectory + @"config.ini";
+
+        public IniManager(string aPath) => Path = aPath;
+
+        public IniManager() { }
+
+        public string Get(string aSection, string aKey) => GetPrivateString(aSection, aKey);
+
+        public void Set(string aSection, string aKey, string aValue)
+        {
+            Create();
+            Set(aSection, aKey, aValue as object);
+        }
+
+        public void Set(string aSection, string aKey, object aValue)
+        {
+	        Create();
+	        WritePrivateString(aSection, aKey, aValue.ToString().Replace("False", "0").Replace("True", "1"));
+        }
+
+        public bool CheckExist() => System.IO.File.Exists(Path);
+
+        public void Create()
+        {
+            if (!CheckExist())
+            {
+                System.IO.File.Create(Path).Close();
+            }
+        }
+
+        #region private
+        private string GetPrivateString(string aSection, string aKey)
+        {
+            var buffer = new StringBuilder(Size);
+            GetPrivateString(aSection, aKey, null, buffer, Size, Path);
+            return buffer.ToString();
+        }
+
+        private void WritePrivateString(string aSection, string aKey, string aValue) =>
+            WritePrivateString(aSection, aKey, aValue, Path);
+
+        private const int Size = 1_024; //Максимальный размер (для чтения значения из файла)
+
+        //Импорт функции GetPrivateProfileString (для чтения значений) из библиотеки kernel32.dll
+        [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
+        private static extern int GetPrivateString(string section, string key, string def, StringBuilder buffer, int size, string path);
+
+        //Импорт функции WritePrivateProfileString (для записи значений) из библиотеки kernel32.dll
+        [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
+        private static extern int WritePrivateString(string section, string key, string str, string path);
+        #endregion
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("wp.dll.imanager")]
+[assembly: AssemblyDescription("2.007")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wins Programs")]
+[assembly: AssemblyProduct("wp.dll.imanager")]
+[assembly: AssemblyCopyright("Голубцов А., Пермь, 2018-2019")]
+[assembly: AssemblyTrademark("x86")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("db5ec20a-d89a-4d38-9723-3eea73e43f6d")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.5")]
+[assembly: AssemblyFileVersion("1.5.2005.1030")]
